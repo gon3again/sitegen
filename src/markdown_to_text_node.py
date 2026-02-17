911a7ca928ae59f141d3db11365f34997996f5d7
gon3again <dustinrose.dr@gmail.com>
2026-02-17 01:21:35 +0100

    split_nodes_link function added

--- src/markdown_to_text_node.py
+++ src/markdown_to_text_node.py
@@ -30,6 +30,59 @@ def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:Tex
         return new_nodes
 
 
+def split_nodes_link(old_nodes:list[TextNode]):
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            links = extract_markdown_links(node.text)
+            split_text = node.text
+            new_link_nodes = []
+
+  
+            for i in range(len(links)):
+                new_link_nodes.append(TextNode(links[i][0], TextType.LINK, links[i][1]))
+                split_text = split_text.replace("["+links[i][0]+"]"+"("+links[i][1]+")","[link]")
+
+
+            split_text = split_text.split("[link]")
+            new_text_nodes = []
+            for t in split_text:
+                
+                if t != "":
+                    new_text_nodes.append(TextNode(t, TextType.TEXT))
+
+            combined_nodes = []
+            combined_nodes.extend(new_text_nodes)
+            combined_nodes.extend(new_link_nodes)
+            
+            '''print(node.text)
+            print(links)
+            print(f"split_text:{split_text} len:{len(split_text)}")
+            print(new_link_nodes)
+            print(new_text_nodes)'''
+            result_nodes = []
+            while len(combined_nodes) > 0:
+                min_index = float("inf")
+                cur_node = None
+                for n in combined_nodes:
+                    if node.text.index(n.text) < min_index:
+                        min_index = node.text.index(n.text)
+                        cur_node = n
+
+                combined_nodes.remove(cur_node)
+                result_nodes.append(cur_node)
+                
+            print(result_nodes)
+
+            
+          
+
+           
+
+    
+
+
+
+
 
 
 def extract_markdown_images(text):
@@ -43,7 +96,19 @@ def extract_markdown_links(text):
 
 
 
-test_text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg |||||This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-print(f"Text:{test_text}")
-print(extract_markdown_images(test_text))
-print(extract_markdown_links(test_text))
+
+
+test_node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+)
+
+split_nodes_link([test_node])
+#print(extract_markdown_links(test_node.text))
+
+
+
+#test_text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg |||||This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+#print(f"Text:{test_text}")
+#print(extract_markdown_images(test_text))
+#print(extract_markdown_links(test_text))
